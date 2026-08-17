b8c348aeb489f7f522a20acacb7da90f0c7cfecc
umerfarooq0320 <uf5757519@gmail.com>
2026-08-17 21:25:39 +0500

    Added script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let user score = 0;
+let computerScore = 0;
+
+const choices =document.querySelector All("choices")
+
+choices.array.forEach(choice => {
+    choice.addEventlistener("click"()=>{
+    console.log("chois=ce was clicked")
+});
+});
